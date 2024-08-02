0fb3bc2a6f8faf963d9861d99ced8a4375e6e978
赵晨炀 <904852749@qq.com>
2024-08-02 15:16:50 +0800

    feat: Rework the tunnel with ssh -R

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"sync"
@@ -11,7 +11,13 @@ import (
 	"github.com/gliderlabs/ssh"
 	"github.com/teris-io/shortid"
 	gossh "golang.org/x/crypto/ssh"
+	"golang.org/x/term"
 )
+
+type Session struct {
+	session ssh.Session
+	port    int
+}
 
 var clients sync.Map
 
@@ -19,18 +25,13 @@ type HttpHandler struct{}
 
 func (h *HttpHandler) handleWebhook(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
-	ch, ok := clients.Load(id)
+	value, ok := clients.Load(id)
 	if !ok {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("client id not found"))
 		return
 	}
-	b, err := io.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer r.Body.Close()
-	ch.(chan string) <- string(b)
+	session := value.(Session)
 }
 
 func startHTTTPServer() error {
@@ -44,8 +45,9 @@ func startHTTTPServer() error {
 
 func startSSHServer() error {
 	sshPort := ":2222"
-
 	handler := NewSSHHandler()
+
+	fwHandler := &ssh.ForwardedTCPHandler{}
 	server := ssh.Server{
 		Addr:    sshPort,
 		Handler: handler.handleSSHSession,
@@ -58,6 +60,18 @@ func startSSHServer() error {
 		},
 		PublicKeyHandler: func(ctx ssh.Context, key ssh.PublicKey) bool {
 			return true
+		},
+		LocalPortForwardingCallback: ssh.LocalPortForwardingCallback(func(ctx ssh.Context, destinationHost string, destinationPort uint32) bool {
+			log.Println("Accepted forward", destinationHost, destinationPort)
+			return true
+		}),
+		ReversePortForwardingCallback: ssh.ReversePortForwardingCallback(func(ctx ssh.Context, destinationHost string, destinationPort uint32) bool {
+			log.Println("Attempt to bind", destinationHost, destinationPort, "granted")
+			return true
+		}),
+		RequestHandlers: map[string]ssh.RequestHandler{
+			"tcpip-forward":        fwHandler.HandleSSHRequest,
+			"cancel-tcpic-forward": fwHandler.HandleSSHRequest,
 		},
 	}
 
@@ -89,33 +103,48 @@ func NewSSHHandler() *SSHHandler {
 }
 
 func (h *SSHHandler) handleSSHSession(session ssh.Session) {
-	cmd := session.RawCommand()
-	if cmd == "init" {
-		id := shortid.MustGenerate()
-		fmt.Println("new init id channel ", id)
-		webhookerURL := "http://localhost:5000/" + id + "/\n\n"
-		resp := fmt.Sprintf(`%swebhook url: %sssh localhost -p 2222 %s | while IFS= read  -r line; do echo "$line" | curl -X POST -H "Content-Type: application/json" -d @- http://localhost:3000/payment/webhook; done%s`,
-			"\n",
-			webhookerURL,
-			id,
-			"\n\n",
-		)
-		session.Write([]byte(resp))
-		respCh := make(chan string)
-		h.channels[id] = respCh
-		clients.Store(id, respCh)
+	if session.RawCommand() == "tunnel" {
+		session.Write([]byte("tunneling traffic..."))
+		<-session.Context().Done()
 		return
 	}
 
-	if len(cmd) > 0 {
-		respCh, ok := h.channels[cmd]
-		if !ok {
-			session.Write([]byte("invaild webhook id\n"))
-			return
+	term := term.NewTerminal(session, "$ ")
+	msg := fmt.Sprintf("%s\n\nWelcome to webhooker!\n\nPlease enter your webhook destination:\n", banner)
+	term.Write([]byte(msg))
+	for {
+		input, err := term.ReadLine()
+		if err != nil {
+			log.Fatal(err)
 		}
-		for data := range respCh {
-			session.Write([]byte(data + "\n"))
+
+		generatedPort := randomPort()
+		id := shortid.MustGenerate()
+		internalSession := Session{
+			session: session,
+			port:    generatedPort,
 		}
+		clients.Store(id, internalSession)
+
+		// http://webhooker.com/dsfadsaadsfgg
+		webhookURL := fmt.Sprintf("http://localhost:5000/%s/", id)
+		command := fmt.Sprintf("\nGenerated webhook: %s\n\nCommand to copy:\nssh -R 127.0.0.1:%d:%s localhost -p 2222 tunnel\n", webhookURL, generatedPort, input)
+		term.Write([]byte(command))
+		return
 	}
 
 }
+
+func randomPort() int {
+	min := 49152
+	max := 65535
+	return min + rand.Intn(max-min+1)
+}
+
+var banner = `
+   _  ____  ___       ____________  __  ______  ____  __ __ __________ 
+  | |/ /\ \/ / |     / / ____/ __ )/ / / / __ \/ __ \/ //_// ____/ __ \
+  |   /  \  /| | /| / / __/ / __  / /_/ / / / / / / / ,<  / __/ / /_/ /
+ /   |   / / | |/ |/ / /___/ /_/ / __  / /_/ / /_/ / /| |/ /___/ _, _/ 
+/_/|_|  /_/  |__/|__/_____/_____/_/ /_/\____/\____/_/ |_/_____/_/ |_|  
+`
